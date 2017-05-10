6a180404a29ac21a38c1cd03ddd1e767b73c6555
Olga <olgapasibayeva98@gmail.com>
2017-05-10 21:13:07 +0300

    View

---
+++ src/ai152/pasibayeva/BrushButton.java
@@ -0,0 +1,7 @@
+package ai152.pasibayeva;
+
+/**
+ * Created by Ольга on 25.04.2017.
+ */
+public class BrushButton {
+}
---
+++ src/ai152/pasibayeva/Controller.java
@@ -0,0 +1,38 @@
+package ai152.pasibayeva;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.GridPane;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+import java.util.function.Predicate;
+
+
+/**
+ * Created by Ольга on 07.04.2017.
+ */
+public class Controller implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+
+    }
+}
+
